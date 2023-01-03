859b503971cdc6d0b8d4756f294d4a53c45584eb
Aaditya Jha <aditya.jha@amniltech.com>
2023-01-03 13:26:55 +0545

    fix: route of token verify file updated

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 require('./webhook_verify')(app);
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
